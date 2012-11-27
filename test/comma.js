f69c4675233ae1bd0bd3bf3bb0ea72063240a6f3
bitoiu <vmrmonteiro@gmail.com>
2012-11-27 21:30:01 +0000

    gitignore rules, jar with first codestyle

---
+++ test/comma.js
@@ -0,0 +1,11 @@
+var magicWords = [ "abracadabra"
+    , "gesundheit"
+    , "ventrilo"
+  ]
+  , spells = { "fireball" : function () { setOnFire() }
+    , "water" : function () { putOut() }
+  }
+  , a = 1
+  , b = "abc"
+  , etc
+  , somethingElse
